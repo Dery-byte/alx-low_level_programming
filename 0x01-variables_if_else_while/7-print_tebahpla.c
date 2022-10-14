e1b2ce70286527cd9d130e7c5729d1290fccc4a0
Dery-byte <immanuelderry31@gmail.com>
2022-10-14 12:29:23 -0700

    Mu c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,35 @@
+nclude <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0
+ *
+ *     */
+
+int main(void)
+
+{
+
+		char ch;
+
+
+
+			for (ch = 'z'; ch >= 'a'; ch--)
+
+						putchar(ch);
+
+
+
+				putchar('\n');
+
+
+
+					return (0);
+
+}
