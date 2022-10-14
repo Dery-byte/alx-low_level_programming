10aa859fe62ce4bc8bf61da25e062f23ce5f8d59
Dery-byte <immanuelderry31@gmail.com>
2022-10-14 12:36:26 -0700

    Ny

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0
+ *
+ *     */
+
+int main(void)
+
+{
+
+		int numeral;
+
+			char letter;
+
+
+
+				for (numeral = 0; numeral < 10; numeral++)
+
+							putchar((numeral % 10) + '0');
+
+
+
+					for (letter = 'a'; letter <= 'f'; letter++)
+
+								putchar(letter);
+
+
+
+						putchar('\n');
+
+
+
+							return (0);
+
+}
