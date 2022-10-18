bbbfae9757d0d6ab86729ad4e44f45296ae8625c
Dery-byte <immanuelderry31@gmail.com>
2022-10-18 14:13:10 -0700

    Hi

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * _abs - computes the abolute value of an integer
+ *
+ *   * @a: the integer to be checked
+ *
+ *    *
+ *
+ *     * Return: absolute value of n
+ *
+ *      */
+
+int _abs(int a)
+
+{
+
+		if (a > 0)
+
+					return (a);
+
+			else
+
+						return (-a);
+
+}
