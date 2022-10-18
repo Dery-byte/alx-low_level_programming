0155a56c9cb85a8e99d0db68bdc317e499e62eb7
Dery-byte <immanuelderry31@gmail.com>
2022-10-18 14:18:01 -0700

    Hi

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ *   *
+ *
+ *    * Return: returns void
+ *
+ *     */
+
+void jack_bauer(void)
+
+{
+
+		int hr, min;
+
+
+
+			for (hr = 0; hr <= 23; hr++)
+
+					{
+
+								for (min = 0; min <= 59; min++)
+
+											{
+
+															_putchar((hr / 10) + '0');
+
+																		_putchar((hr % 10) + '0');
+
+																					_putchar(':');
+
+																								_putchar((min / 10) + '0');
+
+																											_putchar((min % 10) + '0');
+
+																														_putchar('\n');
+
+																																}
+
+									}
+
+}
