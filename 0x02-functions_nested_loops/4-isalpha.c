5f2eb52f3f451446a6037053d8b32aeb4f2e2e3e
Dery-byte <immanuelderry31@gmail.com>
2022-10-18 14:08:00 -0700

    heloo

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * _isalpha - checks for alphabet characters
+ *
+ *   * @c: character to be checked
+ *
+ *    *
+ *
+ *     * Return: returns 1 if c is lowercase else 0
+ *
+ *      */
+
+int _isalpha(int c)
+
+{
+
+		if (((c >= 'a') && (c <= 'z')) ||
+
+							((c >= 'A') && (c <= 'Z')))
+
+					return (1);
+
+			else
+
+						return (0);
+
+}
