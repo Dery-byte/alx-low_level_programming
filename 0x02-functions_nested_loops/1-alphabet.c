f58a6843e53f93e4d37cc5a1f6c763a045241ca8
Dery-byte <immanuelderry31@gmail.com>
2022-10-18 13:52:05 -0700

    My commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * print_alphabet - prints the alphabets in lowercase
+ *
+ *   *
+ *
+ *    * Return: returns void
+ *
+ *     */
+
+void print_alphabet(void)
+
+{
+		char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+		_putchar(ch);
+
+			_putchar('\n');
+
+}
