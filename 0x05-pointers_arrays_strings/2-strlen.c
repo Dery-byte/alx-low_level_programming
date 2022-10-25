e407ab533403f573034e472df8c8e8086555e7b4
Dery-byte <immanuelderry31@gmail.com>
2022-10-25 12:36:15 -0700

    Hello

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * _strlen - returns the length of a string
+ *
+ *   * @s: pointer to a char
+ *
+ *    *
+ *
+ *     * Return: returns length
+ *
+ *      */
+
+int _strlen(char *s)
+
+{
+
+		int l;
+
+
+
+			for (l = 0; *(s + l) != '\0'; l++)
+
+						;
+
+
+
+				return (l);
+
+}
