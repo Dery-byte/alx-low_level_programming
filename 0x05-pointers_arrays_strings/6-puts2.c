f029edca9c01ff86d3c4cf142478a04d8614137d
Dery-byte <immanuelderry31@gmail.com>
2022-10-25 12:45:13 -0700

    Hi

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * puts2 - Prints every other character of a string
+ *
+ *   * @str: The string containing characters.
+ *
+ *    */
+
+void puts2(char *str)
+
+{
+
+		if (str)
+
+				{
+
+							do {
+
+											if (*str)
+
+																_putchar(*str++);
+
+													} while (*str++);
+
+									_putchar('\n');
+
+								}
+
+}
