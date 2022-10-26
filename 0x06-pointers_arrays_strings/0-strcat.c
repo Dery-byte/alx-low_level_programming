e9b4fc9ef96f27f9d96b515273b00100b21dbb7d
Dery-byte <immanuelderry31@gmail.com>
2022-10-26 08:46:19 -0700

    Hello

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * _strcat - concatenates two strings
+ *
+ *   * @dest: the first pointer to the string
+ *
+ *    * @src: the second pointer to the string
+ *
+ *     *
+ *
+ *      * Return: A pointer to the dest
+ *
+ *       */
+
+char *_strcat(char *dest, char *src)
+
+{
+
+		int d_len = 0, i;
+
+
+
+			while (dest[d_len])
+
+					{
+
+								d_len++;
+
+									}
+
+
+
+				for (i = 0; src[i] != 0; i++)
+
+						{
+
+									dest[d_len] = src[i];
+
+											d_len++;
+
+												}
+
+
+
+					dest[d_len] = '\0';
+
+
+
+						return (dest);
+
+}
